cf9939bb4e46c51182280fee02cfee83ea8ac351
jadedrb <jadecdrb@gmail.com>
2024-10-03 14:34:49 -0400

    added a get all todos route and made a request to it from the frontend

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ import cors from 'cors'
 // bring in the function that will make the connection to the database
 import connectDB from './config.js'
 
+// bring in our todo model to interact with database
+import Todo from './models/todoModel.js'
+
 // create our express app
 const app = express()
 
@@ -22,6 +25,20 @@ const PORT = 8080
 // at least one basic route for testing purposes
 app.get('/test', (req, res) => {
     res.json('Hello (from Server)!')
+})
+
+// a route that gets all todos and sends it to client
+app.get('/todos', async (req, res) => {
+    try {
+        // use find method on the model to retrieve all documents from the todos collection
+        const todos = await Todo.find({})
+        console.log('GET /todos')
+        // send those documents to the client
+        res.status(200).json(todos)
+    } catch(e) {
+        console.log(e)
+        res.status(400).json(e)
+    }
 })
 
 // setup our server to listen on a specific port
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [todos, setTodos] = useState([])
+
   useEffect(() => {
     // make initial request to backend on first render
     async function test() {
-      const response = await fetch('http://localhost:8080/test')
+      const response = await fetch('http://localhost:8080/todos')
       const data = await response.json()
       console.log(data)
+      setTodos(data)
     }
     test()
   }, [])
