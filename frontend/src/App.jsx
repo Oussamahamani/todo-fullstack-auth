766821b1ab673714aa0272ab0faab258c57c0b99
jadedrb <jadecdrb@gmail.com>
2024-10-03 15:27:44 -0400

    finished logic for handle submit and post request

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 function App() {
 
   const [todos, setTodos] = useState([])
+  const [input, setInput] = useState('')
 
   useEffect(() => {
     // make initial request to backend on first render
@@ -16,12 +17,48 @@ function App() {
     test()
   }, [])
 
+  function handleChange(e) {
+    setInput(e.target.value)
+  }
+
+  async function handleSubmit(e) {
+    // stop the default behavior of page refresh
+    e.preventDefault()
+
+    // format our data on the frontend to match the schema
+    const todo = {
+      text: input
+    }
+
+    // make the request
+    const response = await fetch('http://localhost:8080/todos', {
+      method: 'POST',
+      body: JSON.stringify(todo),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    // format the new todo that now has the id and completed property
+    const newTodo = await response.json()
+
+    // keep the state in sync with our data
+    setTodos([...todos, newTodo])
+
+    console.log(newTodo)
+
+  }
+
   return (
     <>
       <h1>Todos:</h1>
       <ul>
         {todos.map(todo => <li key={todo._id}>{todo.text}</li>)}
       </ul>
+      <form onSubmit={handleSubmit}>
+        <input value={input} onChange={handleChange} />
+        <button>Add</button>
+      </form>
     </>
   )
 }
