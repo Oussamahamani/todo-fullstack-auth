eca6ca9036183e36bfae4448e921760c730d09d4
jadedrb <jadecdrb@gmail.com>
2024-10-03 12:10:05 -0400

    added a cors middleware to the express server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 // bring in express for our server setup
 import express from 'express'
 
+// bring in cors to help us reach backend routes from frontend
+import cors from 'cors'
+
 // create our express app
 const app = express()
+
+// setup a cors middleware for our express app
+app.use(cors())
 
 // choosing a port 
 const PORT = 8080
